946dedb7128b3b8153b140273c79187c1a0fc38e
kushal <kushalb2005@gmail.com>
2025-05-28 17:03:36 +0530

    added alembic in backend

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,6 +1,6 @@
 from sqlmodel import create_engine, Session
-from backend.app.core.config import DATABASE_URL
-from backend.app.models.model import AchievementInDB, StudentInDB, ResultInDB
+from app.core.config import DATABASE_URL
+from app.models.model import AchievementInDB, StudentInDB, ResultInDB
 
 engine=create_engine(DATABASE_URL, echo=True)
 
